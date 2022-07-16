88163a8dfd00fa8ebac304b7d5aef9bffafb7b30
eshan.one <108228113+eshan-one@users.noreply.github.com>
2022-07-16 22:32:20 +0530

    Create Program_4.c

---
+++ Program_4.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main(){
+  int x=1,n;
+  printf("Enter the value of n:\n");
+  scanf("%d", &n);
+  n*=2;
+  
+  while(x<=n){
+  printf(" \n%d", x);
+  x+=2;
+    
+  }
+  return 0;
+  
+  
+}
