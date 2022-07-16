c3460ab91befdd4c7a489bc2450ff7444dd0d2c1
eshan.one <108228113+eshan-one@users.noreply.github.com>
2022-07-16 22:31:05 +0530

    Create Program_3.c

---
+++ Program_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+  int x,n;
+  printf("Enter the value of n:\n");
+  scanf("%d", &n);
+  x = n;
+  while(x>=1){
+  printf(" \n%d", x);
+  x--;
+    
+  }
+  return 0;
+  
+  
+}
+
