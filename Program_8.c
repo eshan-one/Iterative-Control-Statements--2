1d597fef693bb1f19adf4779b7c708fe535a7d8b
eshan.one <108228113+eshan-one@users.noreply.github.com>
2022-07-16 22:37:31 +0530

    Create Program_8.c

---
+++ Program_8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+  
+  int x=1,n,c;
+  printf("Enter the value of n:\n");
+  scanf("%d", &n);
+  
+  
+  printf("The squares of the first n numbers are:");
+  
+  while(x<=n){
+    c = x*x;
+  printf(" \n%d", c);
+  x++;
+    
+  }
+  return 0;
+  }
+  
+
